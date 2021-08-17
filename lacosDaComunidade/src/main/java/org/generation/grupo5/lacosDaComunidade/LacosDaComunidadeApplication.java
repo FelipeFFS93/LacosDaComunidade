d6a28c22fc131aa55388045aca3b0ab092709b47
janainarodrigues <janaina.rodrigues03@icloud.com>
2021-08-17 12:02:11 -0300

    Entrega Swagger

--- lacosDaComunidade/src/main/java/org/generation/grupo5/lacosDaComunidade/LacosDaComunidadeApplication.java
+++ lacosDaComunidade/src/main/java/org/generation/grupo5/lacosDaComunidade/LacosDaComunidadeApplication.java
@@ -2,12 +2,21 @@ package org.generation.grupo5.lacosDaComunidade;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class LacosDaComunidadeApplication {
+	@GetMapping
+	public ModelAndView swaggerUi() {
+		return new ModelAndView("redirect:/swagger-ui/");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LacosDaComunidadeApplication.class, args);
 	}
-
 }
