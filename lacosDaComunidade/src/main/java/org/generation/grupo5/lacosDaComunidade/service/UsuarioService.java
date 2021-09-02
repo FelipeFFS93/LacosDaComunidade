b185e48082bbd08ef36011c80a350ccae39d94b5
LucasLM1 <lucascorreia325@gmail.com>
2021-09-02 18:35:07 -0300

    Atualização Back-end

--- lacosDaComunidade/src/main/java/org/generation/grupo5/lacosDaComunidade/service/UsuarioService.java
+++ lacosDaComunidade/src/main/java/org/generation/grupo5/lacosDaComunidade/service/UsuarioService.java
@@ -36,8 +36,8 @@ public class UsuarioService {
 			if(encoder.matches(user.get().getSenha(), usuario.get().getSenha())) {
 				
 				String auth = user.get().getUsuario() + ":" + user.get().getSenha();
-				byte[] encodeAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
-				String authHeader = "Basic " + new String(encodeAuth);
+				byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
+				String authHeader = "Basic " + new String(encodedAuth);
 				
 				user.get().setToken(authHeader);
 				user.get().setNome(usuario.get().getNome());
